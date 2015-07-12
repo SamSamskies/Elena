fb41f0cf4f4ffa73b52f1e4bbba08136ed7f3b7f
Steven Miyakawa SAM <samprofessional@gmail.com>
2015-07-11 21:46:06 -0700

    Add in other stages

--- index.ios.js
+++ index.ios.js
@@ -14,6 +14,52 @@ var {
   StatusBarIOS,
   View,
 } = React;
+var stages = {
+  landsEnd: {
+    latitude: 37.767644,
+    longitude:  -122.494835,
+    title: 'Kanye West',
+    subtitle: 'Lands End Stage',
+    rightCalloutAccessory: {
+      url: "http://www.eurweb.com/wp-content/uploads/2012/04/kanye-west-19.jpg",
+      height: 30,
+      width: 30
+    }
+  },
+  sutro: {
+    latitude: 37.769769,
+    longitude: -122.493317,
+    title: 'Chromeo',
+    subtitle: 'Sutro Stage',
+    rightCalloutAccessory: {
+      url: "http://www.brooklynvegan.com/img/bp/chromeo-tour.jpg",
+      height: 30,
+      width: 30
+    }
+  },
+  panhandle: {
+    latitude: 37.770031,
+    longitude:  -122.485402,
+    title: 'Charlie XCX',
+    subtitle: 'Panhandle Stage',
+    rightCalloutAccessory: {
+      url: "http://954live.com/sites/default/files/assets/artist/charli-xcx/charli-xcx-1607027371.jpeg",
+      height: 30,
+      width: 30
+    }
+  },
+  twinPeaks: {
+    latitude: 37.769652,
+    longitude: -122.482421,
+    title: 'TV On The Radio',
+    subtitle: 'Twin Peaks Stage',
+    rightCalloutAccessory: {
+      url: "http://3.bp.blogspot.com/-Wadh5TidhKU/TXBtw3nxQfI/AAAAAAAAASc/R7QDD7bDiWQ/s1600/tv-on-the-radio.jpg",
+      height: 30,
+      width: 30
+    }
+  }
+};
 
 var Elena = React.createClass({
   mixins: [MapboxGLMap.Mixin],
@@ -24,22 +70,12 @@ var Elena = React.createClass({
         longitude: -122.489285
       },
       zoom: 15,
-      annotations: [{
-        latitude: 40.72052634,
-        longitude:  -73.97686958312988,
-        title: 'This is marker 1',
-        subtitle: 'It has a rightCalloutAccessory too',
-        rightCalloutAccessory: {
-            url: 'http://png-3.findicons.com/files/icons/2799/flat_icons/256/gear.png',
-            height: 25,
-            width: 25
-        }
-      },{
-        latitude: 40.714541341726175,
-        longitude:  -74.00579452514648,
-        title: 'Important!',
-        subtitle: 'Neat, this is a subtitle'
-      }]
+      annotations: [
+        stages.landsEnd,
+        stages.sutro,
+        stages.panhandle,
+        stages.twinPeaks
+      ]
     };
   },
   onRegionChange(location) {
@@ -56,6 +92,11 @@ var Elena = React.createClass({
   },
   onRightAnnotationTapped(e) {
     console.log(e);
+  },
+  componentDidUpdate: function() {
+    setTimeout(function() {
+      this.selectAnnotationAnimated(mapRef, 0);
+    }.bind(this), 1000);
   },
   render: function() {
     StatusBarIOS.setHidden(true);
